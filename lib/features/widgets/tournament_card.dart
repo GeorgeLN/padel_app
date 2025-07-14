4d54dabf8f9779b1927d9d94424058add9726eff
GeorgeLN <jelopez0403@gmail.com>
2025-07-14 15:41:33 -0500

    Cambios en tournamente_card

--- lib/features/widgets/tournament_card.dart
+++ lib/features/widgets/tournament_card.dart
@@ -38,7 +38,7 @@ class TournamentCard extends StatelessWidget {
         ),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.15),
+            color: Colors.black.withValues(alpha: 0.15),
             spreadRadius: 1,
             blurRadius: 8,
             offset: const Offset(0, 4),
