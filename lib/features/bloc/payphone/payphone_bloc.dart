3b8d378bc93b1f09f66c73dfcf19d9f4a325d91a
GeorgeLN <jelopez0403@gmail.com>
2025-10-08 23:38:10 -0500

    Se agrega equatable a las dependencias

--- lib/features/bloc/payphone/payphone_bloc.dart
+++ lib/features/bloc/payphone/payphone_bloc.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import '../../../data/models/payphone/payphone_payment_request.dart';
